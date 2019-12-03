d1af51943c3dbb9e90643c4cf0d6dd0d579d6c98
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2019-12-03 11:03:55 -0300

    Resolvendo exercício 10 e seu desafio

--- twitelum/src/App.js
+++ twitelum/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, {Component, Fragment} from 'react';
 import Cabecalho from './components/Cabecalho'
 import NavMenu from './components/NavMenu'
 import Dashboard from './components/Dashboard'
@@ -12,62 +12,88 @@ const isButtonDisabel = (novoTweet) => {
 }
 
 class App extends Component {
-constructor(){
-    super()
-    this.state={
-        novoTweet:'',
-        lista: ['item 01', 'item 02']
+    constructor() {
+        super()
+        this.state = {
+            novoTweet: '',
+            tweets: [],
+            lista: ['item 01', 'item 02']
+        }
     }
-   
-}
 
-  render() {
+    adicionaTweet = (infosDoEvento) => {
 
-    return (
-      <Fragment>
-        <Cabecalho>
-            <NavMenu usuario="@omariosouto" />
-        </Cabecalho>
-        <div className="container">
-            <Dashboard>
-                <Widget>
-                    <form className="novoTweet">
-                        <div className="novoTweet__editorArea">
+        infosDoEvento.preventDefault()
+
+        if (this.state.novoTweet.length > 0) {
+            this.setState({
+                tweets: [this.state.novoTweet, ...this.state.tweets],
+                novoTweet: "",
+            })
+        }
+
+    }
+
+    render() {
+
+        return (
+            <Fragment>
+                <Cabecalho>
+                    <NavMenu usuario="@omariosouto"/>
+                </Cabecalho>
+                <div className="container">
+                    <Dashboard>
+                        <Widget>
+                            <form className="novoTweet" onSubmit={this.adicionaTweet}>
+                                <div className="novoTweet__editorArea">
                             <span className={
-                                `novoTweet__status ${this.state.novoTweet.length > 140 ? ' novoTweet__status--invalido' : ''}` 
+                                `novoTweet__status ${this.state.novoTweet.length > 140 ? ' novoTweet__status--invalido' : ''}`
                             }
-                                >
+                            >
                                 {this.state.novoTweet.length}/140</span>
-                            <textarea className="novoTweet__editor"
-                            value={this.state.novoTweet} 
-                            onChange={(event) => this.setState({novoTweet: event.target.value})}
-                            placeholder="O que está acontecendo?"></textarea>
-                        </div>
-                        <button 
-                        type="submit" 
-                        className="novoTweet__envia"
-                        disabled={isButtonDisabel(this.state.novoTweet)
-                        }
-                        >
-                            Tweetar
-                            </button>
-                    </form>
-                </Widget>
-                <Widget>
-                    <TrendsArea />
-                </Widget>
-            </Dashboard>
-            <Dashboard posicao="centro">
-                <Widget>
-                    <div className="tweetsArea">
-                         {this.state.lista.map((item) => <Tweet name={item} />)}
-                    </div>
-                </Widget>
-            </Dashboard>
-        </div>
-      </Fragment>
-    );
-  }
+                                    <textarea className="novoTweet__editor"
+                                              value={this.state.novoTweet}
+                                              onChange={(event) => this.setState({novoTweet: event.target.value})}
+                                              placeholder="O que está acontecendo?"></textarea>
+                                </div>
+                                <button
+                                    type="submit"
+                                    className="novoTweet__envia"
+                                    disabled={isButtonDisabel(this.state.novoTweet)
+                                    }
+                                >
+                                    Tweetar
+                                </button>
+                            </form>
+                        </Widget>
+                        <Widget>
+                            <TrendsArea/>
+                        </Widget>
+                    </Dashboard>
+                    <Dashboard posicao="centro">
+                        <Widget>
+                            <div className="tweetsArea">
+
+                                {this.state.tweets.length > 0 ? (
+                                    this.state.tweets.map(
+                                        (tweetInfo, index) => {
+                                            return (
+                                                <Tweet
+                                                    key={tweetInfo + index}
+                                                    texto={tweetInfo}
+                                                />
+                                            )
+                                        }
+                                    )
+                                ) : "Nenhum Tweet encontrado, crie o seu primeiro Tweet"}
+
+                            </div>
+                        </Widget>
+                    </Dashboard>
+                </div>
+            </Fragment>
+        );
+    }
 }
 
 export default App;
