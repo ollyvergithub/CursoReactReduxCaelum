9498e3697fceb9635680c27d0d37c6a0548dc702
Ollyver Ottoboni <ollyverottoboni@gmail.com>
2019-12-10 09:39:09 -0300

    Resolvendo exercício 19 - Componente InputFormField como const fora da classe - state sem this - static contextType = NotificacaoContext - this.setaState com passagem de função this.setState({values}, () => { this.formValidations() })

--- twitelum/src/pages/LoginPage/index.js
+++ twitelum/src/pages/LoginPage/index.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React, {Component, Fragment} from 'react'
 import Cabecalho from '../../components/Cabecalho'
 import Widget from '../../components/Widget'
 import {NotificacaoContext} from "../../context/NotificacaoContext"
@@ -6,26 +6,85 @@ import LoginService from "../../services/LoginService"
 
 import './loginPage.css'
 
+const InputFormField = ({tipo, id, label, errors, values, onChange}) => {
+
+    return (
+
+        <div className="loginPage__inputWrap">
+
+            <label className="loginPage__label" htmlFor={id}>
+                {label}
+            </label>
+
+            <input
+                className="loginPage__input"
+                type={tipo}
+                id={id}
+                name={id}
+                value={values[id]}
+                onChange={onChange}
+            />
+
+
+
+            <p style={{color: "red"}}>{errors[id] && errors[id]}</p>
+
+
+        </div>
+    )
+}
+
 class LoginPage extends Component {
 
     static contextType = NotificacaoContext
 
-    fazerLogin = (infosDoEvento) =>{
+    state = {
+        values: {
+            inputLogin: '',
+            inputSenha: ''
+        },
+
+        errors: {}
+    }
+
+    formValidations = () => {
+
+        console.log()
+
+        const {inputLogin, inputSenha} = this.state.values;
+        const errors = {}
+
+        if (!inputLogin) errors.inputLogin = "O campo Login é Obrigatório"
+        if (!inputSenha) errors.inputSenha = "Esse campo é obrigatório"
+
+        this.setState({errors})
+    }
+
+
+    onFormFieldChange = ({target}) => {
+        const value = target.value;
+        const name = target.name;
+        const values = {...this.state.values, [name]: value};this.setState({values}, () => {
+            this.formValidations();
+        });
+
+    };
+
+
+    fazerLogin = (infosDoEvento) => {
         infosDoEvento.preventDefault()
-        
 
         const dadosLogin = {
-            login: this.refs.inputLogin.value,
-            senha: this.refs.inputSenha.value
+            login: this.state.values.inputLogin,
+            senha: this.state.values.inputSenha,
         }
 
         LoginService.logar(dadosLogin)
             .then(() => {
                 this.context.setMsg("Bem vindo ao Twitelum, login efetuado com sucesso")
             }).catch(erro => {
-                this.context.setMsg(`[Erro ${erro.status}] - ${erro.message}`)
+            this.context.setMsg(`[Erro ${erro.status}] - ${erro.message}`)
         })
-
 
 
     }
@@ -33,23 +92,33 @@ class LoginPage extends Component {
     render() {
         return (
             <Fragment>
-                <Cabecalho />
+                <Cabecalho/>
                 <div className="loginPage">
                     <div className="container">
                         <Widget>
                             <h2 className="loginPage__title">Seja bem vindo!</h2>
                             <form onSubmit={this.fazerLogin} className="loginPage__form" action="/">
-                                <div className="loginPage__inputWrap">
-                                    <label className="loginPage__label" htmlFor="login">Login</label> 
-                                    <input ref="inputLogin" className="loginPage__input" type="text" id="login" name="login"/>
-                                </div>
-                                <div className="loginPage__inputWrap">
-                                    <label className="loginPage__label" htmlFor="senha">Senha</label> 
-                                    <input ref="inputSenha"  className="loginPage__input" type="password" id="senha" name="senha"/>
-                                </div>
-                                {/* <div className="loginPage__errorBox">
-                                    Mensagem de erro!
-                                </div> */}
+
+                                <InputFormField
+                                    tipo={"text"}
+                                    id="inputLogin"
+                                    label="Login: "
+                                    onChange={this.onFormFieldChange}
+                                    values={this.state.values}
+                                    errors={this.state.errors}
+                                />
+
+                                <InputFormField
+                                    tipo={"password"}
+                                    id="inputSenha"
+                                    label="Senha: "
+                                    onChange={this.onFormFieldChange}
+                                    values={this.state.values}
+                                    errors={this.state.errors}
+                                />
+
+
+
                                 <div className="loginPage__inputWrap">
                                     <button className="loginPage__btnLogin" type="submit">
                                         Logar
